f94afe11afb08b9016b36d21e7bf26c05337e80b
John Bradley <jrb@turrettech.com>
2014-09-25 14:43:14 -0500

    vov

--- gen-xml.py
+++ gen-xml.py
@@ -22,7 +22,7 @@ def gen_bin_attr(prop, arch):
         'size': str(os.path.getsize(props.file_path(prop, arch)))
     }
 
-def gen_xml(root_dir):
+def gen_xml(root_dir, out):
     from collections import defaultdict
     branches = defaultdict(list)
 
@@ -33,9 +33,9 @@ def gen_xml(root_dir):
 
     from xml.etree import ElementTree as ET
 
-    branchesEle = ET.Element('branches')
+    branches_ele = ET.Element('branches')
     for k, v in branches.items():
-        bEle = ET.SubElement(branchesEle, 'branch', { 'name': k})
+        bEle = ET.SubElement(branches_ele, 'branch', { 'name': k})
         w32 = ET.SubElement(bEle, 'win')
         w64 = ET.SubElement(bEle, 'win64')
         for p in v:  # snicker
@@ -44,7 +44,11 @@ def gen_xml(root_dir):
             r64 = ET.SubElement(w64, 'revision', gen_rev_attr(p, 'x64'))
             ET.SubElement(r64, 'binary', gen_bin_attr(p, 'x64'))
 
-    ET.dump(branchesEle)
+    with open(out, 'w') as output:
+        output.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?>')
+        output.write('<?xml-stylesheet type="text/xsl" href="assets/obsbuilds.xsl"?>')
+        ET.ElementTree(branches_ele).write(output, xml_declaration=False, encoding='utf-8')
+
 
 import argparse
 parser = argparse.ArgumentParser(description='ce.org builds xml gen')
@@ -52,7 +56,7 @@ parser.add_argument('-d', '--root-dir', dest='root', default='.')
 parser.add_argument('-o', '--out-file', dest='out', default='builds.xml')
 args = parser.parse_args()
 
-gen_xml(args.root)
+gen_xml(args.root, args.out)
 
 
 
